8bf807f7b093eb77be678ffb42f061962ceae64b
BouriauB <47318623+BouriauB@users.noreply.github.com>
2021-02-19 11:53:48 +0100

    Add new page '/add-country' & new component 'CountryListItem'

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import Home from '../views/Home.vue'
+import AddCountry from '../views/AddCountry.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -11,6 +12,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/home',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/add-country',
+    name: 'AddCountry',
+    component: AddCountry
   },
   {
     path: '/message/:id',
---
+++ src/views/AddCountry.vue
@@ -0,0 +1,65 @@
+<template>
+    <ion-page>
+        <ion-header :translucent="true">
+            <ion-toolbar>
+                <ion-title>Inbox</ion-title>
+            </ion-toolbar>
+        </ion-header>
+
+        <ion-content :fullscreen="true">
+            <ion-refresher slot="fixed" @ionRefresh="refresh($event)">
+                <ion-refresher-content></ion-refresher-content>
+            </ion-refresher>
+
+            <ion-header collapse="condense">
+                <ion-toolbar>
+                    <ion-title size="large">Inbox</ion-title>
+                </ion-toolbar>
+            </ion-header>
+
+            <ion-list>
+                <CountryListItem v-for="country in countries" :key="country.alpha2Code" :country="country" />
+            </ion-list>
+        </ion-content>
+    </ion-page>
+</template>
+
+<script lang="ts">
+    import { IonContent, IonHeader, IonList, IonPage, IonRefresher, IonRefresherContent, IonTitle, IonToolbar } from '@ionic/vue';
+    import CountryListItem from '@/components/CountryListItem.vue';
+    import { defineComponent } from 'vue';
+    import { getMessages } from '@/data/messages';
+    import axios from 'axios';
+
+    export default defineComponent({
+        name: 'AddCountry',
+        data() {
+            return {
+                countries: null as any
+            }
+        },
+        mounted () {
+            axios
+                .get('https://restcountries.eu/rest/v2/all')
+                .then(response => (this.countries = response.data))
+        },
+        methods: {
+            refresh: (ev: CustomEvent) => {
+                setTimeout(() => {
+                    ev.detail.complete();
+                }, 3000);
+            }
+        },
+        components: {
+            IonContent,
+            IonHeader,
+            IonList,
+            IonPage,
+            IonRefresher,
+            IonRefresherContent,
+            IonTitle,
+            IonToolbar,
+            CountryListItem
+        },
+    });
+</script>
